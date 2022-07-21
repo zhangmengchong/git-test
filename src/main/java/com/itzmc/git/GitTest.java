e80af1f4477806eb4a59c35af2ecc459a4e884ad
itzmc <zmc@163.com>
2022-07-21 20:06:08 +0800

    second commit

--- src/main/java/com/itzmc/git/GitTest.java
+++ src/main/java/com/itzmc/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itzmc.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git !");
+        System.out.println("hello,Git 2 !");
     }
 }
