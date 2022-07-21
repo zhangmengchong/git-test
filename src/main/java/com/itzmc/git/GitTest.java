90b6ef30a40e7f13beee1e84581ac23e437ec710
itzmc <zmc@163.com>
2022-07-21 20:21:01 +0800

    hot fix first commit

--- src/main/java/com/itzmc/git/GitTest.java
+++ src/main/java/com/itzmc/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,Git !");
         System.out.println("hello,Git 2 !");
         System.out.println("hello,Git 3 !");
+        System.out.println("hello,hot fix !");
     }
 }
