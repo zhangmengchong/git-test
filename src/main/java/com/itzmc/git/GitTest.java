0658526ca728ba803aec2d618675c1984a21ac53
itzmc <zmc@163.com>
2022-07-21 21:10:11 +0800

    push test ...

--- src/main/java/com/itzmc/git/GitTest.java
+++ src/main/java/com/itzmc/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,hot fix !");
         System.out.println("hello,master 01 !");
         System.out.println("hello,hot fix 02 !");
+        System.out.println("push test ... ");
     }
 }
