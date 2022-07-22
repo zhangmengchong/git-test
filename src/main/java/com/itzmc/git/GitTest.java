cac20fec5635c350e0a048f74e3d65cd6d99251d
zhangmengchong <109740242+zhangmengchong@users.noreply.github.com>
2022-07-22 10:25:44 +0800

    gitee copy

--- src/main/java/com/itzmc/git/GitTest.java
+++ src/main/java/com/itzmc/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,hot fix 02 !");
         System.out.println("push test ... ");
         System.out.println("pull test ... ");
+        System.out.println("gitee copy ");
     }
 }
